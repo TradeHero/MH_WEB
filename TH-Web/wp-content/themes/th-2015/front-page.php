7181032b5b7ce2bb7f7a3a32c16bf04eb22f83b2
Albert Camacho <albert@tradehero.mobi>
2015-02-25 10:13:46 +0800

    Testing mutiple content blocks plugin

--- TH-Web/wp-content/themes/th-2015/front-page.php
+++ TH-Web/wp-content/themes/th-2015/front-page.php
@@ -1,190 +1,44 @@
-<?php get_header(); ?>
+<?php 
+/* Front page template */
+get_header(); ?>
 
-<div id="intro-wrap">
-	<div id="intro" class="preload" data-autoplay="false" data-navigation="true" data-pagination="true" data-transition="fade">	
-		<div class="intro-item" style="background-color: #1376FB; background-image: url('<?php bloginfo('template_url'); ?>/assets/img/bg-home.jpg');">
-			<div class="intro-mockup-wrapper">
-				<div class="caption-mockup caption-left column six">
-					<h2>Trade better together</h2>
-					<p>Join the global movement of over a million users that empowers you to invest.</p>
-					<p>Practice risk free using real market data, share insights, and earn cash, while having fun everyday!</p>
-					<a id="cta-apple" class="button" href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/btn-download-apple.png" /></a><a id="cta-google" class="button" href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/btn-download-google.png" /></a>
-				</div><!-- caption -->												
-				<div class="intro-mockup intro-right column six last">
-					<img src="<?php bloginfo('template_url'); ?>/assets/img/phones.png" alt="">
-				</div><!-- intro-mockup -->	
-			</div><!-- intro-mockup-wrapper -->
-		</div>
-		<div class="intro-item" style="background-color: #70767C;">
-			<div class="intro-mockup-wrapper">						
-				<div class="caption-mockup">
-					<h2>Learn to Trade</h2>
-					<p>Learn winning strategies from TradeHeroes. Keep track of their investments and trading insights.</p>
-				</div><!-- caption -->	
-				<div class="intro-mockup">
-					<img src="http://placehold.it/1193x937/ddd/fff&text=Beetle%20image" alt="">
-				</div><!-- intro-mockup -->	
-			</div><!-- intro-mockup-wrapper -->										
-		</div>
-		<div class="intro-item" style="background-color: #70767C;">
-			<div class="intro-mockup-wrapper">						
-				<div class="caption-mockup">
-					<h2>Make Money</h2>
-					<p>Trade well, share your insights and make money from subscriptions of Premium Followers.</p>
-				</div><!-- caption -->	
-				<div class="intro-mockup">
-					<img src="http://placehold.it/1193x937/ddd/fff&text=Beetle%20image" alt="">
-				</div><!-- intro-mockup -->	
-			</div><!-- intro-mockup-wrapper -->										
-		</div>	
-	</div><!-- intro -->
-</div><!-- intro-wrap -->
-
-<div id="main">
-	<section class="row section">
-		<div class="row-content buffer even clear-after">
-			<div class="section-title">
-				<h3>Why play TradeHero?</h3>
+<div id="home">	
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<div id="intro-wrap">
+		<div id="intro" class="preload" data-autoplay="false" data-navigation="true" data-pagination="true" data-transition="fade">	
+			<div class="intro-item" style="background-color: #1376FB; background-image: url('<?php bloginfo('template_url'); ?>/assets/img/bg-home.jpg');">
+				<div class="intro-mockup-wrapper">
+					<div class="caption-mockup caption-left column six">
+						<h2>Trade better together</h2>
+						<p>Join the global movement of over a million users that empowers you to invest.</p>
+						<p>Practice risk free using real market data, share insights, and earn cash, while having fun everyday!</p>
+						<a id="cta-apple" class="button" href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/btn-download-apple.png" /></a><a id="cta-google" class="button" href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/btn-download-google.png" /></a>
+					</div><!-- caption -->												
+					<div class="intro-mockup intro-right column six last">
+						<img src="<?php bloginfo('template_url'); ?>/assets/img/phones.png" alt="">
+					</div><!-- intro-mockup -->	
+				</div><!-- intro-mockup-wrapper -->
 			</div>
-			<div class="column three centertxt">
-				<div class="big-icon red"><i class="icon icon-tumbler"></i></div>
-				<div class="big-icon-text clear-after">
-					<h4>Practice</h4>
-					<p class="text-s">Start with 100,000 virtual dollars and practice trading risk-free using real market data.</p>
-				</div>
+		</div><!-- intro -->
+	</div><!-- end #intro-wrap -->
+	<div id="main">
+		<section class="row section">
+			<div class="row-content buffer even clear-after">				
+				<p><?php the_content(); ?></p>
 			</div>
-			<div class="column three centertxt">
-				<div class="big-icon green"><i class="icon icon-crown"></i></div>
-				<div class="big-icon-text clear-after">
-					<h4>Compete</h4>
-					<p class="text-s">Pit your strategies against your friends and members from across the world and top the leaderboard.</p>
-				</div>
-			</div>						
-			<div class="column three centertxt">
-				<div class="big-icon blue"><i class="icon icon-users"></i></div>
-				<div class="big-icon-text clear-after">
-					<h4>Follow</h4>
-					<p class="text-s">Follow expert traders (Heroes) and learn their trading strategies real-time.</p>
-				</div>
+		</section>
+		<section class="row section">
+			<div class="row-content buffer-top buffer-bottom clear-after">
+				<?php the_block( 'Features1' ); ?>
 			</div>
-			<div class="column three last centertxt">
-				<div class="big-icon"><i class="icon icon-money"></i></div>
-				<div class="big-icon-text clear-after">
-					<h4>Earn</h4>
-					<p class="text-s">Become a Hero and earn money from your followers. Win <strong>real cash prizes</strong> in sponsored competitions!</p>
-				</div>
+		</section>
+		<section class="row section">
+			<div class="row-content buffer-top buffer-bottom clear-after">
+				<?php the_block( 'Features2' ); ?>
 			</div>
-		</div>
-	</section>
-
-	<section class="row section">
-		<div class="row-content buffer-top buffer-bottom clear-after">
-			<div class="column four">
-				<div class="small-icon red"><i class="icon icon-globe"></i></div>
-				<div class="small-icon-text clear-after">
-					<h4>Global Coverage</h4>
-					<p class="text-xs">Trade across 33 exchanges globally.</p>
-				</div>
-			</div>
-			<div class="column four">
-				<div class="small-icon red"><i class="icon icon-comments"></i></div>
-				<div class="small-icon-text clear-after">
-					<h4>Social</h4>
-					<p class="text-xs">Discuss investment strategies and sentiment with the global TradeHero community.</p>
-				</div>
-			</div>
-			<div class="column four last">
-				<div class="small-icon red"><i class="icon icon-graph"></i></div>
-				<div class="small-icon-text clear-after">
-					<h4>Analysis</h4>
-					<p class="text-xs">Leverage targeted, bite-sized news and analysis from our leading content partners to enhance your alpha.</p>
-				</div>
-			</div>
-			<div class="column four">
-				<div class="small-icon red"><i class="icon icon-diamond"></i></div>
-				<div class="small-icon-text clear-after">
-					<h4>Achievements</h4>
-					<p class="text-xs">Collect badges as you complete key goals on your way to becoming a Trade Hero.</p>
-				</div>
-			</div>
-			<div class="column four">
-				<div class="small-icon red"><i class="icon icon-megaphone"></i></div>
-				<div class="small-icon-text clear-after">
-					<h4>Stock Alerts</h4>
-					<p class="text-xs">Monitor the price movements of your favourite stocks.</p>
-				</div>
-			</div>
-			<div class="column four last">
-				<div class="small-icon red"><i class="icon icon-eye"></i></div>
-				<div class="small-icon-text clear-after">
-					<h4>Watchlists</h4>
-					<p class="text-xs">Watch how a portfolio performs before deciding to go all in.</p>
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<section class="row section">
-		<div class="slogan animation">
-			<h2>TradeHero Academy</h2>	
-			<p>Bite-sized educational videos</p>
-		</div><!-- slogan -->
-		<div class="column half push-fourth reset">
-			<iframe width="560" height="315" src="//www.youtube.com/embed/ehyM9gWy6V0" frameborder="0" allowfullscreen></iframe>
-		</div>
-	</section>
-
-
-	<section class="row section text-light" style="background-color:#3BAFDA;">
-		<div class="row-content buffer even clear-after">
-			<div class="section-title"><h3>What they say about TradeHero</h3></div>
-			<div class="testimonial-slider" data-autoplay="5000" data-pagination="true" data-transition="fade" data-autoheight="false">
-				<div class="quote">
-					<div class="column two">
-						<figure class="testimonial-img">
-							<img src="http://placehold.it/600x600/ddd/fff&text=Beetle%20image" alt="">
-						</figure>
-					</div><!-- column -->
-					<div class="column ten last">
-						<p>"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."</p>
-						<div class="author">Helen Beck</div>
-					</div><!-- column -->
-				</div><!-- quote -->
-				<div class="quote">
-					<div class="column two">
-						<figure class="testimonial-img">
-							<img src="http://placehold.it/600x600/ddd/fff&text=Beetle%20image" alt="">
-						</figure>
-					</div><!-- column -->
-					<div class="column ten last">								
-						<p>"Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."</p>
-						<div class="author">Craig Edwards</div>
-					</div><!-- column -->
-				</div><!-- quote -->		
-				<div class="quote">
-					<div class="column two">
-						<figure class="testimonial-img">
-							<img src="http://placehold.it/600x600/ddd/fff&text=Beetle%20image" alt="">
-						</figure>
-					</div><!-- column -->
-					<div class="column ten last">									
-						<p>"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour."</p>
-						<div class="author">Michael Dean</div>
-					</div><!-- column -->									
-				</div><!-- quote -->							
-			</div><!-- testimonial-slider -->				
-		</div>
-	</section>					
-
-	<section class="row section text-light call-to-action" style="background: #6CD106;">
-		<div class="row-content buffer even">
-			<h3>Play TradeHero now!</h3>
-			<a id="cta-apple" class="button" href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/btn-download-apple.png" /></a><a id="cta-google" class="button" href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/btn-download-google.png" /></a>
-		</div>
-	</section>								
-</div><!-- id-main -->
-	
-			
-
-
+		</section>	
+	</div><!-- End #main -->
+	<?php endwhile; ?>
+	<?php endif; ?>
+</div><!-- end #home -->	
 <?php get_footer(); ?>
