c6ef2570ab465e83e17d22c20b3f9078934dd034
Albert Camacho <albert@tradehero.mobi>
2015-03-04 17:48:52 +0800

    Single-column front page hero banner

--- TH-Web/wp-content/themes/th-2015/front-page.php
+++ TH-Web/wp-content/themes/th-2015/front-page.php
@@ -5,19 +5,15 @@ get_header(); ?>
 <div id="home">	
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<div id="intro-wrap">
-		<div id="intro" class="preload" data-autoplay="false" data-navigation="true" data-pagination="true" data-transition="fade">	
+		<div id="intro" class="preload" data-autoplay="5000" data-navigation="true" data-pagination="true" data-transition="fadeUp">
 			<div class="intro-item" style="background-color: #1376FB; background-image: url('<?php bloginfo('template_url'); ?>/assets/img/bg-home.jpg');">
-				<div class="intro-mockup-wrapper">
-					<div class="caption-mockup caption-left column six">
-						<h2>Trade better together</h2>
-						<p>Join the global movement of over a million users that empowers you to invest.</p>
-						<p>Practice risk free using real market data, share insights, and earn cash, while having fun everyday!</p>
-						<a id="cta-apple" class="button" href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/btn-download-apple.png" /></a><a id="cta-google" class="button" href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/btn-download-google.png" /></a>
-					</div><!-- caption -->												
-					<div class="intro-mockup intro-right column six last">
-						<img src="<?php bloginfo('template_url'); ?>/assets/img/phones.png" alt="">
-					</div><!-- intro-mockup -->	
-				</div><!-- intro-mockup-wrapper -->
+				<div class="caption">
+					<h2>Trade Better, Together</h2>
+					<p>Join the global movement of over a million users that empowers you to invest.<br />
+					Practice risk free using real market data, share insights, and earn cash, while having fun everyday!</p>
+					<a id="cta-apple" class="button" href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/btn-download-apple.png" /></a><a id="cta-google" class="button" href="#"><img src="<?php bloginfo('template_url'); ?>/assets/img/btn-download-google.png" /></a>
+					
+				</div><!-- caption -->
 			</div>
 		</div><!-- intro -->
 	</div><!-- end #intro-wrap -->
